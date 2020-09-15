68966b887ce89fea19a76f9d5583a1791a2f0cbb
akarimin <ali.nouri@paradigmquest.com>
2020-09-14 23:48:56 -0400

    week3: convex hull

---
+++ src/main/java/edu/akarimin/week2/ConvexHull.java
@@ -0,0 +1,15 @@
+package edu.akarimin.week2;
+
+/**
+ * A set of n points is the smallest perimeter fence enclosing the points.
+ * پوسته کوژ (قشر محدب)
+ * Convex,Hull,Vertex
+ *
+ * Graham Scan:
+ * 1. Start from smallest y-coordinate point and give index to all points counterclockwise. (polar angle)
+ * 2. Always you can move counterclockwise from lowest vertex.
+ *
+ * Solution for 1: Define a total order comparing by y-coordinates.
+ */
+public class ConvexHull {
+}
