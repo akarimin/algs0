b42f91071fcc144bcf8f79f95275360e2949101a
akarimin <ali.nouri@paradigmquest.com>
2020-09-20 20:26:43 -0400

    week3: Stability

--- src/main/java/edu/akarimin/week2/queues/Deque.java
+++ src/main/java/edu/akarimin/week2/queues/Deque.java
@@ -1,4 +1,4 @@
-package edu.akarimin.week2.deque;
+package edu.akarimin.week2.queues;
 
 import edu.princeton.cs.algs4.StdOut;
 
---
+++ src/main/java/edu/akarimin/week3/Stability.java
@@ -0,0 +1,9 @@
+package edu.akarimin.week3;
+
+/**
+ * Maintaining multiple sorts called stability.
+ * A stable sort preserves the relative order of items with equal keys.
+ * Insertion and merge sorts are stable but not selection and shell sorts.
+ */
+public class Stability {
+}
