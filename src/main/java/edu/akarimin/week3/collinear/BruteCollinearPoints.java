ea93ea5d4a7767c4b57397b216da31b95c41820e
akarimin <ali.nouri@paradigmquest.com>
2020-09-21 23:02:03 -0400

    week3: assignment api

--- src/main/java/edu/akarimin/week3/collinear/BruteCollinearPoints.java
+++ src/main/java/edu/akarimin/week3/collinear/BruteCollinearPoints.java
@@ -1,4 +1,17 @@
 package edu.akarimin.week3.collinear;
 
 public class BruteCollinearPoints {
+
+    public BruteCollinearPoints(Point[] points) {   // finds all line segments containing 4 points
+
+    }
+
+    public int numberOfSegments() {                 // the number of line segments
+        return 0;
+    }
+
+    public LineSegment[] segments() {               // the line segments
+       return null;
+    }
 }
+
--- src/main/java/edu/akarimin/week3/collinear/Point.java
+++ src/main/java/edu/akarimin/week3/collinear/Point.java
@@ -22,17 +22,21 @@ public class Point implements Comparable<Point> {
 
     public String toString() {                  // string representation
 
+        return null;
     }
 
     public int compareTo(Point that) {          // compare two points by y-coordinates, breaking ties by x-coordinates
 
+        return 0;
     }
 
     public double slopeTo(Point that) {         // the slope between this point and that point
 
+        return 0;
     }
 
     public Comparator<Point> slopeOrder() {     // compare two points by slopes they make with this point
 
+        return null;
     }
 }
