1f3d26c62c912a0ccafd5e273610f833a8c222be
akarimin <ali.nouri@paradigmquest.com>
2020-09-26 18:50:44 -0400

    week-3: collinear

--- src/main/java/edu/akarimin/week3/collinear/Point.java
+++ src/main/java/edu/akarimin/week3/collinear/Point.java
@@ -2,12 +2,16 @@ package edu.akarimin.week3.collinear;
 
 import java.util.Comparator;
 
-public class Point implements Comparable<Point> {
+public final class Point implements Comparable<Point> {
 
     private final int x;
     private final int y;
 
     public Point(final int x, final int y) {                // constructs the point (x, y)
+        if (x < 0 || x > 32767)
+            throw new IllegalArgumentException("x is invalid: " + x);
+        if (y < 0 || y > 32767)
+            throw new IllegalArgumentException("y is invalid: " + y);
         this.x = x;
         this.y = y;
     }
@@ -20,23 +24,32 @@ public class Point implements Comparable<Point> {
 
     }
 
-    public String toString() {                  // string representation
-
-        return null;
-    }
-
     public int compareTo(Point that) {          // compare two points by y-coordinates, breaking ties by x-coordinates
-
-        return 0;
+        int diff = this.y - that.y;
+        if (diff <= 0) {
+            diff = this.x - that.x;
+        }
+        return diff;
     }
 
     public double slopeTo(Point that) {         // the slope between this point and that point
-
-        return 0;
+        int yDiff = that.y - this.y;
+        int xDiff = that.x - this.x;
+        if (xDiff == 0 && yDiff == 0)
+            return Double.NEGATIVE_INFINITY;
+        else if (yDiff == 0)
+            return 0;
+        else if (xDiff == 0)
+            return Double.POSITIVE_INFINITY;
+        else
+            return yDiff / xDiff;
     }
 
     public Comparator<Point> slopeOrder() {     // compare two points by slopes they make with this point
+        return (o1, o2) -> (int) o1.slopeTo(o2);
+    }
 
-        return null;
+    public String toString() {                  // string representation
+        return "{x='" + x + "','y='" + y + "'";
     }
 }
