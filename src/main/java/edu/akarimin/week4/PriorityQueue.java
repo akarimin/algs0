5cd85dc2d39beb84f4a0f035df43eca00bb88426
akarimin <ali.nouri@paradigmquest.com>
2020-10-25 21:31:04 -0400

    week-4: priority-queue api

--- src/main/java/edu/akarimin/week4/PriorityQueue.java
+++ src/main/java/edu/akarimin/week4/PriorityQueue.java
@@ -1,4 +1,41 @@
 package edu.akarimin.week4;
 
-public class PriorityQueue {
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * Remove the largest or smallest item.
+ * API is similar to Stack or Queue with difference in Generics of Comparable for Keys.
+ * Different impl: (Order of Growth)
+ * 1. Sort           -> MLogN (Time)  N (Space)
+ * 2. Elementary PQ  -> MN            M (Space) [Unordered and Ordered]
+ * 3. Binary Heap    -> NLogM         M (Space)
+ * 4. Best in Theory -> N             M
+ * Implement with LinkedList or Resizing Array
+ */
+public class PriorityQueue<T extends Comparable<T>> {
+
+    private int size;
+
+    public void insert(T item) {
+        // different implementations
+    }
+
+    public void delMin() {
+        // different implementations
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<String> pq = new PriorityQueue<>();
+        while (StdIn.hasNextLine()) {
+            String line = StdIn.readLine();
+            pq.insert(line);
+            if (pq.size() > 5) {
+                pq.delMin();
+            }
+        }
+    }
 }
