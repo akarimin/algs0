82d1a42547d6c3ad7f5949924e8412dd1b154e53
akarimin <ali.nouri@paradigmquest.com>
2020-11-01 15:14:28 -0500

    week-4: binary-heap

--- src/main/java/edu/akarimin/week4/BinaryHeap.java
+++ src/main/java/edu/akarimin/week4/BinaryHeap.java
@@ -1,8 +1,5 @@
 package edu.akarimin.week4;
 
-import static edu.akarimin.week2.InsertionSort.exchange;
-import static edu.akarimin.week2.InsertionSort.less;
-
 /**
  * Non-basic implementation of PriorityQueue
  * Binary-Tree -> Complete Tree
@@ -14,6 +11,15 @@ import static edu.akarimin.week2.InsertionSort.less;
  * 2. Parent of node at k is at k/2 (integer divide).
  * 3. Children of node k are at 2k and 2k+1.
  * Peter Principle; Node promoted to level of incompetence. (not be better than its boss)
+ * both insert and delMax -> logN order of growth ! which is great.
+ * Fibonnaci insert is 1 for inserting and logN for delMax but not as practical as binary heap.
+ * Assumptions:
+ * -> client does not change the keys while they are on the PQ.
+ * Best Practice: Make Keys Immutable.
+ * underflow: throw exception in case of empty PQ.
+ * overflow: add no-arg constructor and use resizing array,
+ * For minimum-oriented priority queue, use greater() helper operation instead of less().
+ * We can add other operations like Remove an arbitrary item OR change the priority of an item.
  */
 public class BinaryHeap<Key extends Comparable<Key>> {
 
@@ -21,43 +27,57 @@ public class BinaryHeap<Key extends Comparable<Key>> {
     private int n;
 
     @SuppressWarnings("unchecked")
-    public BinaryHeap(int capacity) {
-        this.pq = (Key[]) new Comparable[capacity];
-    }
-
-    // Peter Principle
-    public void swim(int k) {
-        while (k > 1 && less(k/2, k)) {   // k/2 = parent
-            exchange(pq, k, k/2);
-            k = k/2;
-        }
+    public BinaryHeap(int capacity) {                      // we can use resizing strategy instead of fixed capacity
+        this.pq = (Key[]) new Comparable[capacity + 1];    // bc we don't use position 0
     }
 
     // Add node at the end, then swim it up
     public void insert(Key x) {
-       pq[++n] = x;
-       swim(n);
-    }
-
-    // if a new boss is hired and is less than subordinates, it should swim down (sink), starting with greater child
-    private void sink(int k) {
-        while (2*k <= n) {
-            int j = 2*k;
-            if (j < n && less(j, j + 1))    // children of node k (subordinates)
-                j++;
-            if (!less(k, j))
-                break;
-            exchange(pq, k, j);
-            k = j;
-        }
+        pq[++n] = x;
+        swim(n);
     }
 
     // brilliant
     public Key delMax() {
         Key max = pq[1];
-        exchange(pq, 1, n--);       // violates heap order so we need a sink
+        exchange(1, n--);       // violates heap order so we need a sink
         sink(1);
         pq[n + 1] = null;             // prevent loitering
         return max;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    // Peter Principle
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {   // k/2 = parent
+            exchange(k, k / 2);
+            k = k / 2;
+        }
+    }
+
+    // if a new boss is hired and is less than subordinates, it should swim down (sink), starting with greater child
+    private void sink(int k) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(j, j + 1))    // children of node k (subordinates)
+                j++;
+            if (!less(k, j))
+                break;
+            exchange(k, j);
+            k = j;
+        }
+    }
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exchange(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
     }
 }
