dd4047aa68ab5a441cb8c299c358b9b97c6403cd
akarimin <ali.nouri@paradigmquest.com>
2021-02-20 18:16:38 -0500

    week-5: SymbolTable api

---
+++ src/main/java/edu/akarimin/week5/SymbolTable.java
@@ -0,0 +1,79 @@
+package edu.akarimin.week5;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Objects;
+
+/**
+ * Assumptions:
+ * 1. Keys are Comparable.
+ * 2. Keys are generic type, use equals and hashcode.
+ * 3. Use immutable types for symbol table keys.
+ * */
+public class SymbolTable<Key, Value> {
+
+    // To create a ST
+    public SymbolTable() {
+    }
+
+    // remove key from table if value is absent, overrides old value with new value if duplicate
+    public void put(Key key, Value value) {
+
+    }
+
+    // null if key is absent
+    public Value get(Key key) {
+        return null;
+    }
+
+    // lazy version of delete
+    public void delete(Key key) {
+        put(key, null);
+    }
+
+    // is there a value paired with key ?
+    public boolean contains(Key key) {
+        return Objects.nonNull(get(key));
+    }
+
+    public void isEmpty() {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public Iterable<Key> keys() {
+        return null;
+    }
+
+    /**
+     * Reflexive, Symmetric, Transitive, Non-null.
+     */
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object that) {
+        // Check that all significant fields are the same.
+        if (this == that)
+            return true;
+        if (that == null)
+            return false;
+        if (this.getClass() != that.getClass())
+            return false;
+        SymbolTable<Key, Value> st = (SymbolTable<Key, Value>) that;
+        return Objects.equals(this, st);
+    }
+
+    public static void main(String[] args) {
+        SymbolTable<String, Integer> st = new SymbolTable<>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+        for (String s: st.keys()) {
+            StdOut.println(s + " " + st.get(s));
+        }
+    }
+
+}
